acc96032eceaa72cf4f48b59027ec3481500bb94
Jin Park <jinpark1504@gmail.com>
2024-05-20 14:13:12 +0900

    Clear

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -8,35 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let slider = UISlider()
-    let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        label.text = "value: 0.5"
-        
-        slider.minimumValue = 0
-        slider.maximumValue = 1
-        slider.value = 0.5
-        slider.isContinuous = true
-        slider.addAction(UIAction {[weak self] _ in
-            self?.label.text = "value: \(String(format:"%.1f", self?.slider.value ??  0))"
-        }, for: .valueChanged)
-        
-        let stackView = UIStackView(arrangedSubviews: [label, slider])
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.alignment = .fill
-        
-        view.addSubview(stackView)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.widthAnchor.constraint(equalToConstant: 200)
-        ])
     }
 }
 
