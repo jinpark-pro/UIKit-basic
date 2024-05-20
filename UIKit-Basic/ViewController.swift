8451b1e8c7e86a9fde848ebcf97ff411ffcb849d
Jin Park <jinpark1504@gmail.com>
2024-05-20 17:35:21 +0900

    Image, scaleAspectFit vs scaleAspectFill

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -11,6 +11,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let imageView = UIImageView(image: UIImage(systemName: "hare.fill"))
+        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
+        imageView.tintColor = .purple
+        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView)
+        
+        let flagImageView = UIImageView(image: UIImage(systemName: "flag"))
+        flagImageView.frame = CGRect(x: 50, y: 200, width: 100, height: 50)
+        flagImageView.clipsToBounds = true
+        flagImageView.contentMode = .scaleAspectFill
+        view.addSubview(flagImageView)
     }
 }
 
