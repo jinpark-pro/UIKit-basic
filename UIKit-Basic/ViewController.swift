e4de1d5f210fbe00230f6c1911fafe665c7fe2ac
Jin Park <jinpark1504@gmail.com>
2024-05-20 16:12:30 +0900

    Clear

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -11,26 +11,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label = UILabel()
-        label.text = "Stylized Text Stylized Text Stylized Text"
-        label.font = UIFont.boldSystemFont(ofSize: 17)
-        label.textColor = .yellow
-        label.backgroundColor = .red
-        label.textAlignment = .center
-        
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.widthAnchor.constraint(equalToConstant: 200),
-            label.heightAnchor.constraint(equalToConstant: 40),
-        ])
     }
 }
 
