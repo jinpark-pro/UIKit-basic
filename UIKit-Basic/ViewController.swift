0dc8693acdcf2eab9d599400087cc9d76a4f1df9
Jin Park <jinpark1504@gmail.com>
2024-05-20 16:42:08 +0900

    Clear

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -10,21 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let attributedString = NSMutableAttributedString(string: "Hello, World!")
-        attributedString.addAttribute(.font, value: UIFont.italicSystemFont(ofSize: 20), range: NSRange(location: 0, length: 5))
-        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: NSRange(location: 7, length: 5))
-        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: 12))
         
-        let label = UILabel()
-        label.attributedText = attributedString
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
-        ])
     }
 }
 
