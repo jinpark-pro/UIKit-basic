529a45915abc9ae0c47831b5d9257f6669007b85
Jin Park <jinpark1504@gmail.com>
2024-05-20 11:39:21 +0900

    StackView

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        
+        let topLabel = UILabel()
+        topLabel.text = "위"
+        topLabel.textAlignment = .center
+        topLabel.backgroundColor = .lightGray
+        
+        let leftLabel = UILabel()
+        leftLabel.text = "왼쪽"
+        leftLabel.textAlignment = .center
+        leftLabel.backgroundColor = .lightGray
+        
+        let rightLabel = UILabel()
+        rightLabel.text = "오른쪽"
+        rightLabel.textAlignment = .center
+        rightLabel.backgroundColor = .lightGray
+        
+        let hStackView = UIStackView(arrangedSubviews: [leftLabel, rightLabel])
+        hStackView.axis = .horizontal
+        hStackView.spacing = 10
+        
+        stackView.addArrangedSubview(topLabel)
+        stackView.addArrangedSubview(hStackView)
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
+            stackView.heightAnchor.constraint(equalToConstant: 100),
+        ])
     }
 }
 
