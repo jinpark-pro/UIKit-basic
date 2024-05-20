f45735fdd9ce86f70e590a400d5efef6f4c2f78e
Jin Park <jinpark1504@gmail.com>
2024-05-20 17:10:09 +0900

    Clear

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -10,33 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.spacing = 8
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let imageView = UIImageView(image: UIImage(systemName: "creditcard"))
-        imageView.contentMode = .scaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: 30),
-            imageView.heightAnchor.constraint(equalToConstant: 30)
-        ])
-        stackView.addArrangedSubview(imageView)
-        
-        let label = UILabel()
-        label.text = "Credit Card"
-        label.font = UIFont.systemFont(ofSize: 17)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubview(label)
-        
-        view.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 250)
-        ])
     }
 }
 
