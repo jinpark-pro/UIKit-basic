26fd4bb401d81d40bf08c4f05ff493fdba2c4621
Jin Park <jinpark1504@gmail.com>
2024-05-20 14:22:47 +0900

    Spacer

--- UIKit-Basic/ViewController.swift
+++ UIKit-Basic/ViewController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let labelLeft = UILabel()
+        labelLeft.text = "Left"
+        labelLeft.backgroundColor = .lightGray
+        
+        let labelRight = UILabel()
+        labelRight.text = "Right"
+        labelRight.backgroundColor = .lightGray
+        
+        let spacer = UIView()
+        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        spacer.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        stackView.addArrangedSubview(labelLeft)
+        stackView.addArrangedSubview(spacer)
+        stackView.addArrangedSubview(labelRight)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+        ])
     }
 }
 
